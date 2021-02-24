8d5654383c615972fbbbaf155583e5a3a8a177f6
AaquibSyed <m.nsyedaaquib@gmail.com>
2021-02-24 14:22:48 +0530

    initiated

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,9 +1,22 @@
 import "./About.css";
-
 import React from "react";
+import WorkIcon from "@material-ui/icons/Work";
+import SchoolIcon from "@material-ui/icons/School";
 
 function About() {
-  return <div className="about">Under development</div>;
+  return (
+    <div className="about__main">
+      <div className="about__intro">
+        <div className="intro__text">
+          <h1 className="text__main">Hi, I Am Aaquib</h1>
+          <h6 className="tex__sub">A Passionate software developer</h6>
+        </div>
+        <div className="intro__pic">
+          <img src="./greyDp.jpg" alt="greyDp" />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default About;
